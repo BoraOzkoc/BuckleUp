bdd8851e0b76f5f87763006accd568f6fa50ad6e
Bora Özkoç <boraozkoc@hotmail.com>
2023-11-06 14:52:35 +0300

    DoTween & Area Controller & Intractable added

---
+++ Assets/Scripts/Area/AreaController.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AreaController : MonoBehaviour,IInteractable
+{
+    [SerializeField] private Image _fillImage;
+    [SerializeField] private bool _playerEntered;
+    private Tween _fillTween;
+
+    private void Awake()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        _fillImage.fillAmount = 0;
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<PlayerMovement>(out PlayerMovement playerMovement))
+        {
+            AreaEntered();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.TryGetComponent<PlayerMovement>(out PlayerMovement playerMovement))
+        {
+            AreaExit();
+        }
+    }
+
+    private void TriggerArea()
+    {
+        
+    }
+    public void AreaEntered()
+    {
+        _playerEntered = true;
+        _fillTween = _fillImage.DOFillAmount(1, 1f).OnComplete(TriggerArea);
+    }
+
+    public void AreaExit()
+    {
+        _playerEntered = false;
+        _fillImage.DOKill();
+        _fillImage.fillAmount = 0;
+    }
+}
